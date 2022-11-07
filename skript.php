b7be95277dea88f9e499ce131ce6761417465b54
Jamames <johan.james.jj@gmail.com>
2022-11-07 10:51:13 +0100

    first commit

---
+++ skript.php
@@ -0,0 +1,28 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "login";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+$login_success = false;
+$full_name = "";
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+		if($row["username"] == $_POST["username"] &&
+					$row["password"] == $_POST["password"]) {
+                        $login_success = true;
+                        echo "welcome";}
+	}
+    if ($login_success == false) {
+        echo "fuck off";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
